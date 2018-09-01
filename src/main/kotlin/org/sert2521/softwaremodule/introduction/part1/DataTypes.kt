1b732dc051da015e2739881f86a5c10abcb3561e
Aidan Heller <asfheller@gmail.com>
2018-09-01 03:23:28 -0700

    Fix another silly directory

--- src/main/kotlin/org/sert2521/softwaremodule/introduction/part1/DataTypes.kt
+++
@@ -1,5 +0,0 @@
-package org.sert2521.softwaremodule.introduction.part1
-
-fun main(args: Array<String>) {
-    var name = ""
-}
---
+++ src/main/kotlin/org/sert2521/softwaremodule/part1/DataTypes.kt
@@ -0,0 +1,5 @@
+package org.sert2521.softwaremodule.part1
+
+fun main(args: Array<String>) {
+    var name = ""
+}
