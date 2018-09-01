21259786169d2994c3b7a4e6535a867a6c9599ab
Aidan Heller <asfheller@gmail.com>
2018-09-01 03:50:10 -0700

    Add Operators exercise

---
+++ src/main/kotlin/org/sert2521/softwaremodule/part1/Operators.kt
@@ -0,0 +1,12 @@
+package org.sert2521.softwaremodule.part1
+
+fun main(args: Array<String>) {
+    // Here's an example of using operators.
+    val nine = 9
+    val ten = 10
+    println(nine + ten)
+    println(nine + ten == 21)
+    println("Nine plus ten equals twenty-one is a " + (nine + ten == 21) + " statement.")
+
+    // Write your own code here!
+}
