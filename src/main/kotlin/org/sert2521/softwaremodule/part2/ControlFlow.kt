d6b0ac91dca2d95606e15ea61a4df9dc4c6bd339
Aidan Heller <asfheller@gmail.com>
2018-09-27 08:45:41 -0700

    Add ControlFlow exercise

---
+++ src/main/kotlin/org/sert2521/softwaremodule/part2/ControlFlow.kt
@@ -0,0 +1,10 @@
+package org.sert2521.softwaremodule.part2
+
+// Declare your variable here
+
+fun main(args: Array<String>) {
+    // Write your if statement here
+
+    // Write your while loop here
+
+}
